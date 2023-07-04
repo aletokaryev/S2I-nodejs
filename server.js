4fc70d88b4fa5c015109d86dbcf5438977acfe47
aletokaryev <alessandrotokaryev060@gmail.com>
2023-07-04 19:30:14 +0200

    Little fix

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.use('/api/v1', require('./routes/courseRoutes'))
 app.use('/api/v1', require('./routes/typologyRoutes'))
 app.use('/api/v1', require('./routes/universityRoutes'))
 
-mongoose.connect("mongodb+srv://admin:adminpass@cluster0.hm0tetn.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+connectToDB_string = "mongodb+srv://admin:adminpass@cluster0.hm0tetn.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.connect(connectToDB_string, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(app.listen(port, () => console.log(`Connected to database and listening on localhost:${port}!`)))
 .catch(err => console.error('Error when connecting.', err))
